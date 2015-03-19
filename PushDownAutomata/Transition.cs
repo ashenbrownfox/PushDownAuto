bbb4e1fec37b85849a2d0e95db138830cec63105
ashenbrownfox <newhoopsdynasty@gmail.com>
2015-03-19 10:51:09 -0400

    Initial Commit

---
+++ PushDownAutomata/Transition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushDownAutomata
+{
+    public class Transition
+    {
+        public string StartState { get; private set; }
+        public char Symbol { get; private set; }
+        public string EndState { get; private set; }
+
+        /**
+         * Basic Constructor initializes the StartState, endState and symbol
+         * **/
+        public Transition(string startState, char symbol, string endState)
+        {
+            StartState = startState;
+            Symbol = symbol;
+            EndState = endState;
+        }
+
+        /**
+         * This method will make it easier later on for printing out the states.
+         * **/
+        public override string ToString()
+        {
+            return string.Format("({0}, {1}) -> {2}", StartState, Symbol, EndState);
+        }
+    }
+}
---
+++ PushDownAutomata/UserUtility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PushDownAutomata
+{
+    public class UserUtility
+    {
+        /**
+         * THis method is used for Fails
+         * @param String message
+         * **/
+        string path = ".//..//..//..//";
+        public UserUtility()
+        {
+
+        }
+        public void readInputFile()
+        {
+            Console.WriteLine("Please type the name of the input file.");
+            string input_file = Console.ReadLine();
+            FileStream fs_in = new FileStream(path + input_file, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr_in = new StreamReader(fs_in);
+
+            sr_in.Close();
+            fs_in.Close();
+        }
+        public void FailMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Write(message);
+        }
+        /**
+         * THis method is used for Success
+         * @param String message
+         * **/
+        public void SuccessMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Write(message);
+        }
+        /**
+         * Typical write method
+         * @param String message
+         * **/
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
